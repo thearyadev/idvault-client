bf0ecc6ff40a8ec2c97b5dd36a663ef48cf565b2
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-04-02 11:14:31 -0400

    idk

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,15 @@
-import { Link, router } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Pressable, Button } from "react-native";
+import { Link, router } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { Button, Text, View } from 'react-native';
 
-export default function AppHome() {
+export default function App() {
   return (
     <View>
-      <Text>Welcome to IDVault. This is the landing page.</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
       <View>
-          <Button title="Go to homepage!" onPress={() => router.navigate("/home/home")}/>
+        <Button title="Go to homepage!" onPress={() => router.navigate("/home/home")}/>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({},);
