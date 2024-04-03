630f04bab0d7cac49ef20b068badbff93f3524ed
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-04-03 11:28:53 -0400

    add api implementations

--- lib/api.ts
+++ lib/api.ts
@@ -1,14 +1,39 @@
-import { GenericDocument, Token } from "./types";
+import { GenericDocument, Token, UserDetails } from "./types";
 
-const API_URL: string = "http://localhost:3000"; // will be changed in production
+const API_URL: string = "http://192.168.1.160:3000"; // will be changed in production
 
-
-export function login(): Token {
-  throw new Error("Not implemented");
+export async function login(
+  username: string,
+  password: string,
+): Promise<Token> {
+  const requestBody = new URLSearchParams();
+  requestBody.append("username", username);
+  requestBody.append("password", password);
+  const request = await fetch(`${API_URL}/users/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: requestBody.toString(),
+  });
+  if (!request.ok) {
+    return Promise.reject("Failed to login");
+  }
+  const token = (await request.text()) as Token;
+  return token;
 }
 
-export function userDetails(token: Token) {
-  throw new Error("Not implemented");
+export async function userDetails(token: Token): Promise<UserDetails> {
+  const request = await fetch(`${API_URL}/users/me`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!request.ok) {
+    return Promise.reject("Failed to get user details");
+  }
+  return (await request.json()) as UserDetails;
 }
 
 export function getDocument(token: Token, documentId: number): GenericDocument {
