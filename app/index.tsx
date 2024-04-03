c0ece0ce7245cdf9ea37a99ccd2af36b325c03ba
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-04-03 11:28:42 -0400

    redirect on load to auth page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,6 @@
-import { Link, router } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { Button, Text, View } from 'react-native';
+import React from 'react';
+import { Redirect } from 'expo-router';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <View>
-        <Button title="Go to homepage!" onPress={() => router.navigate("/home/home")}/>
-      </View>
-    </View>
-  );
+  return <Redirect href="/authentication/login" /> 
 }
