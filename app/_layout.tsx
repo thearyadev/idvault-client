04d40ed0f654746991c3d82ba774991782d75c26
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-04-04 20:37:30 -0400

    add stack views

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,51 @@
-import Navbar from "components/navbar/navbar";
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 import { View } from "react-native";
 import { StyleSheet } from "react-native";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Navbar from "components/navbar/navbar";
+
 export default function BaseLayout() {
+  // AsyncStorage.clear()
   return (
     <>
-      <View style={styles.container}>
-        <Slot />
-      </View>
+      <Stack>
+        <Stack.Screen
+          name="home/home"
+          options={{
+            headerShown: false,
+            gestureEnabled: false,
+          }}
+        />
+        <Stack.Screen
+          name="home/search/index"
+          options={{
+            headerShown: false,
+            gestureEnabled: false,
+          }}
+        />
+        <Stack.Screen
+          name="home/my_account/index"
+          options={{
+            headerShown: false,
+            gestureEnabled: false,
+          }}
+        />
+        <Stack.Screen
+          name="home/about_idvault/index"
+          options={{
+            headerShown: false,
+            gestureEnabled: false,
+          }}
+        />
+
+
+
+        <Stack.Screen name="home/view_document" options={{presentation: "modal", headerShown: false}}/>
+        <Stack.Screen name="home/add_document" options={{presentation: "modal", headerShown: false}}/>
+      </Stack>
       <Navbar />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#0C4D7D",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
