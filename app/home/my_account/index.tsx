9bcee3cdba824d60c2462c1efca956e34781af09
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-04-06 13:25:13 -0400

    add logout button

--- app/home/my_account/index.tsx
+++ app/home/my_account/index.tsx
@@ -1,9 +1,19 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import ButtonLarge from "components/buttons/button_large";
 import WhiteText from "components/text/white_text";
 import Content from "components/wrappers/content";
+import { router } from "expo-router";
 export default function MyAccountScreen() {
   return (
     <Content>
       <WhiteText>My Account</WhiteText>
+      <ButtonLarge
+        label="Logout"
+        onPress={() => {
+          AsyncStorage.clear().catch()
+          router.navigate("authentication/login");
+        }}
+      />
     </Content>
   );
 }
