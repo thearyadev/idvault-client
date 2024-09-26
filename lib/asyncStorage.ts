73df2eef39e08a565b2d11fe41c9a845f8613878
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-09-25 21:53:59 -0400

    add storage functions for encryption keys

--- lib/asyncStorage.ts
+++ lib/asyncStorage.ts
@@ -15,3 +15,19 @@ export async function getUsersName() {
 export async function setUsersName(name: string) {
   await AsyncStorage.setItem("name", name);
 }
+
+export async function setPublicKey(publicKey: string) {
+  await AsyncStorage.setItem("publicKey", publicKey);
+}
+
+export async function getPublicKey() {
+  return await AsyncStorage.getItem("publicKey");
+}
+
+export async function setPrivateKey(privateKey: string) {
+  await AsyncStorage.setItem("privateKey", privateKey);
+}
+
+export async function getPrivateKey() {
+  return await AsyncStorage.getItem("privateKey");
+}
