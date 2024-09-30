16efd35fc11bbf4e9691ec96f7a389886bdca092
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-09-30 15:32:31 -0400

    generate or retrieve keys when app loads

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Redirect } from 'expo-router';
+import { generateEncryptionKeys, loadKeys, saveKeys } from 'lib/encryption';
 
 export default function App() {
+  loadKeys().then((keys) => {
+    if (!keys) {
+      const newKeys = generateEncryptionKeys()
+      saveKeys(newKeys)
+    }
+  })
   return <Redirect href="/authentication/login" /> 
 }
