6c427e7016c7f98c35775eebc8806088a60a3ddc
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-10-06 19:30:21 -0400

    add: default styles

---
+++ components/styles/bottomSheetStyles.tsx
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const bottomSheetStyles = StyleSheet.create({
+  bottomSheet: {
+    padding: 16,
+    backgroundColor: "white",
+  },
+});
---
+++ components/styles/buttonStyle.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const buttonStyle = StyleSheet.create({
+  buttonStyle: {
+    width: "100%",
+    height: 50,
+    borderRadius: 6,
+    backgroundColor: "#2b2bb3",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
